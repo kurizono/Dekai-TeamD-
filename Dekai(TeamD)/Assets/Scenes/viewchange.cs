f959052be28d045ef43b738059624fa209c9d967
yuki <57346951+kurizono@users.noreply.github.com>
2020-11-13 21:11:21 +0900

    [add]方角を変える

--- Dekai(TeamD)/Assets/Scenes/viewchange.cs
+++ Dekai(TeamD)/Assets/Scenes/viewchange.cs
@@ -7,17 +7,23 @@ using UnityEngine.UI;
 public class viewchange : MonoBehaviour
 {
     //ゲームオブジェクト、ボタンの設定
-    public GameObject white, black, blue, red;
+    public GameObject north, east, south, west;
     public Button Left, Right;
-    
+
+    int compass = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        //ボタンの有効・無効設定
-        white.SetActive(true);
-        black.SetActive(false);
-        blue.SetActive(false);
-        red.SetActive(false);
+        //有効・無効設定
+        north.SetActive(true);
+        east.SetActive(false);
+        south.SetActive(false);
+        west.SetActive(false);
+        
+        //ボタンを押した時の動作
+        Left.onClick.AddListener(Left_Click);
+        Right.onClick.AddListener(Right_Click);
     }
 
     // Update is called once per frame
@@ -26,33 +32,81 @@ public class viewchange : MonoBehaviour
         //矢印キーを押した時の動作
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            white.SetActive(false);
-            black.SetActive(true);
-            blue.SetActive(false);
-            red.SetActive(false);
-            Debug.Log("black");
+            north.SetActive(false);
+            east.SetActive(true);
+            south.SetActive(false);
+            west.SetActive(false);
+            Debug.Log("east");
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            white.SetActive(false);
-            black.SetActive(false);
-            blue.SetActive(true);
-            red.SetActive(false);
-            Debug.Log("blue");
+            north.SetActive(false);
+            east.SetActive(false);
+            south.SetActive(true);
+            west.SetActive(false);
+            Debug.Log("south");
         }
         else if(Input.GetKeyDown(KeyCode.UpArrow))
         {
-            white.SetActive(false);
-            black.SetActive(false);
-            blue.SetActive(false);
-            red.SetActive(true);
+            north.SetActive(false);
+            east.SetActive(false);
+            south.SetActive(false);
+            west.SetActive(true);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            white.SetActive(true);
-            black.SetActive(false);
-            blue.SetActive(false);
-            red.SetActive(false);
+            north.SetActive(true);
+            east.SetActive(false);
+            south.SetActive(false);
+            west.SetActive(false);
+        }
+    }
+
+    //左ボタン押したらどうなるか
+    private void Left_Click()
+    {
+        compass = (compass + 3) % 4;
+        Compass_Check();
+    }
+    //右ボタン押したらどうなるか
+    private void Right_Click()
+    {
+        compass = (compass + 1) % 4;
+        Compass_Check();
+    }
+    //コンパスを確認して、向きを変えたい
+    private void Compass_Check()
+    {
+        switch (compass)
+        {
+            case 0:
+                north.SetActive(true);
+                east.SetActive(false);
+                south.SetActive(false);
+                west.SetActive(false);
+                Debug.Log("north");
+                break;
+            case 1:
+                north.SetActive(false);
+                east.SetActive(true);
+                south.SetActive(false);
+                west.SetActive(false);
+                Debug.Log("east");
+                break;
+            case 2:
+                north.SetActive(false);
+                east.SetActive(false);
+                south.SetActive(true);
+                west.SetActive(false);
+                Debug.Log("south");
+                break;
+            case 3:
+                north.SetActive(false);
+                east.SetActive(false);
+                south.SetActive(false);
+                west.SetActive(true);
+                Debug.Log("west");
+                break;
         }
     }
 }
