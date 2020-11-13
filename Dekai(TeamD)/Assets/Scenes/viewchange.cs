74343bb0424ca259fd642704a9129a2ad6e583e3
VitaminB2 <kotarookine@gmail.com>
2020-11-13 21:12:16 +0900

    【追加】矢印クリックすると向きが変わる

--- Dekai(TeamD)/Assets/Scenes/viewchange.cs
+++ Dekai(TeamD)/Assets/Scenes/viewchange.cs
@@ -7,52 +7,74 @@ using UnityEngine.UI;
 public class viewchange : MonoBehaviour
 {
     //ゲームオブジェクト、ボタンの設定
-    public GameObject white, black, blue, red;
+    public GameObject North, East, South, West;
     public Button Left, Right;
+
+    int compass = 0;
     
     // Start is called before the first frame update
     void Start()
     {
         //ボタンの有効・無効設定
-        white.SetActive(true);
-        black.SetActive(false);
-        blue.SetActive(false);
-        red.SetActive(false);
+        North.SetActive(true);
+        East.SetActive(false);
+        South.SetActive(false);
+        West.SetActive(false);
+        //ボタンを押したときの動作
+        Left.onClick.AddListener(Left_Click);
+        Right.onClick.AddListener(Right_Click);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //矢印キーを押した時の動作
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        
+     
+    }
+    //ボタンを押したらどうなるか
+    private void Left_Click()
+    {
+        compass = (compass + 3) % 4;
+        CompassCheck();
+    }
+    private void Right_Click()
+    {
+        compass = (compass + 1) % 4;
+        CompassCheck();
+    }
+    //コンパスを確認して向きを変えたい
+    private void CompassCheck()
+    {
+        switch (compass)
         {
-            white.SetActive(false);
-            black.SetActive(true);
-            blue.SetActive(false);
-            red.SetActive(false);
-            Debug.Log("black");
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            white.SetActive(false);
-            black.SetActive(false);
-            blue.SetActive(true);
-            red.SetActive(false);
-            Debug.Log("blue");
-        }
-        else if(Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            white.SetActive(false);
-            black.SetActive(false);
-            blue.SetActive(false);
-            red.SetActive(true);
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            white.SetActive(true);
-            black.SetActive(false);
-            blue.SetActive(false);
-            red.SetActive(false);
+            case 0:
+                North.SetActive(true);
+                East.SetActive(false);
+                South.SetActive(false);
+                West.SetActive(false);
+                Debug.Log("North");
+                break;
+            case 1:
+                North.SetActive(false);
+                East.SetActive(true);
+                South.SetActive(false);
+                West.SetActive(false);
+                Debug.Log("East");
+                break;
+            case 2:
+                North.SetActive(false);
+                East.SetActive(false);
+                South.SetActive(true);
+                West.SetActive(false);
+                Debug.Log("South");
+                break;
+            case 3:
+                North.SetActive(false);
+                East.SetActive(false);
+                South.SetActive(false);
+                West.SetActive(true);
+                Debug.Log("West");
+                break;
         }
     }
 }
