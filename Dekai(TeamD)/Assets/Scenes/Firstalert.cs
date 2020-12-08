761cf4c65f41e30bf22bf49af97302386dc45385
yuki <57346951+kurizono@users.noreply.github.com>
2020-12-08 21:54:22 +0900

    [add]タイトルのボタンとクレジットを追加、最初のalertを改良

--- Dekai(TeamD)/Assets/Scenes/Firstalert.cs
+++ Dekai(TeamD)/Assets/Scenes/Firstalert.cs
@@ -7,6 +7,9 @@ using UnityEngine.SceneManagement;
 public class Firstalert : MonoBehaviour
 {
     public Text alert;
+    //ここの数をいじると、表示の秒数等が変わるようにしました(1,3,4なら、1秒で透明から不透明、1秒から3秒まで不透明、3秒から4秒までで不透明から透明)
+    private int[] alerttime = new int[3] {1,3,4};
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,22 +19,25 @@ public class Firstalert : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.time < 1)
+        //0からalerttime[0]秒まで
+        if (Time.time < alerttime[0])
         {
-            alert.color = new Color(1, 1, 1, 1 - Mathf.Sin(Time.time * Mathf.PI / 2 + Mathf.PI / 2));
+            alert.color = new Color(1, 1, 1, alerttime[0] - (Mathf.Sin(Time.time * Mathf.PI / 2 + Mathf.PI / 2)) / alerttime[0]);
             //Sin(90) = 1 → sin(0) = 0
         }
         //不透明
-        else if (Time.time < 2)
+        //alerttime[0]からalerttime[1]秒まで
+        else if (Time.time < alerttime[1])
         {
 
         }
         //不透明から透明
-        else if (Time.time < 3)
+        //alerttime[1]秒からalerttime[2]秒まで
+        else if (Time.time < alerttime[2])
         {
-            alert.color = new Color(1, 1, 1, 1 - Mathf.Sin((Time.time - 2) * Mathf.PI / 2));
+            alert.color = new Color(1, 1, 1, (alerttime[2] - alerttime[1]) - (Mathf.Sin((Time.time - alerttime[1]) * Mathf.PI / 2))/(alerttime[2] - alerttime[1]));
         }
-        if (Time.time > 3)
+        if (Time.time > alerttime[2])
         {
             //シーンを遷移するプログラム(事前にusing UnityEngine.SceneManagementを書いておく必要あり)
             SceneManager.LoadScene("title");
---
+++ Dekai(TeamD)/Assets/Scenes/TitleButton.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleButton : MonoBehaviour
+{
+    private GameObject start_object, story_object, credit_object, return_object;
+    public GameObject credittext_object;
+    public Button start_button, story_button, credit_button, return_button;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirstPosi();
+        start_button.onClick.AddListener(Start_Click);
+        story_button.onClick.AddListener(Story_Click);
+        credit_button.onClick.AddListener(Credit_Click);
+        return_button.onClick.AddListener(Return_Click);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FirstPosi()
+    {
+        start_object = start_button.gameObject;
+        story_object = story_button.gameObject;
+        credit_object = credit_button.gameObject;
+        return_object = return_button.gameObject;
+        start_object.SetActive(true);
+        story_object.SetActive(true);
+        credit_object.SetActive(true);
+        credittext_object.SetActive(false);
+        return_object.SetActive(false);
+    }
+
+
+
+    private void Start_Click()
+    {
+        SceneManager.LoadScene("game");
+    }
+    private void Story_Click()
+    {
+
+    }
+    private void Credit_Click()
+    {
+        start_object.SetActive(false);
+        story_object.SetActive(false);
+        credit_object.SetActive(false);
+        credittext_object.SetActive(true);
+        return_object.SetActive(true);
+    }
+    private void Return_Click()
+    {
+        FirstPosi();
+    }
+}
